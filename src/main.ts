210c214b98e051202d6bc431d4b9dde2ca5ea4c8
Sogon-Njugush <josephnjuguna00@gmail.com>
2026-01-03 23:25:32 +0400

    set main.ts credentials  to false

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,23 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable Validation
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   // Enable CORS (Allows frontend at localhost:3000 to hit this API)
   app.enableCors({
     origin: ['http://localhost:3000', 'https://ho-pn-frontend.vercel.app'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    credentials: true,
+    credentials: false,
     allowedHeaders: ['Content-Type'],
   });
 
   // Listen on Port 4000
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(Number(process.env.PORT));
 }
 bootstrap();
